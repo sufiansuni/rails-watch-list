dfe4ed553d2e3f586625607c921eebd0bb32b616
Sufian Suni <sufiansuni@gmail.com>
2021-11-25 12:08:48 +0800

    model rspec cleared, revised

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,6 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, length: { minimum: 6 }
-  validates :movie_id, uniqueness: { scope: :list_id }
-  validates :list_id, uniqueness: { scope: :movie_id }
+  validates :movie, uniqueness: { scope: :list }
 end
