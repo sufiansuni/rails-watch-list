28159d4b8a57e61b3dfbae97e3e5aeafa49306cf
Sufian Suni <sufiansuni@gmail.com>
2021-11-25 13:20:49 +0800

    initial routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: ["index", "show"]
+  resources :lists, only: %w[index show new create] do
+    resources :bookmarks, only: %w[new create]
+  end
+
+  resources :bookmarks, only: %w[destroy]
 end
