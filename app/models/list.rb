d4ae385ba35ff7c479077bb5afc8b0107808738a
Sufian Suni <sufiansuni@gmail.com>
2021-11-25 20:38:10 +0800

    bookmarks done

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,5 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks
 
   validates :name, presence: true, uniqueness: true
+  # validates :image_url, url: true, allow_blank: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists, only: %w[index show new create] do
-    resources :bookmarks, only: %w[new create]
+    resources :bookmarks, only: %w[create]
   end
 
   resources :bookmarks, only: %w[destroy]
